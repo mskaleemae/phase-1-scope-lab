41c9099d6bf70b585dc6dfe526a48ed9e872c9c0
Kaeli Gilmore <mskaleemae2000@gmail.com>
2022-08-30 15:26:33 -0400

    commit tree 21109c2894b901fd71df7792f883039a16a2b6c0

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+const leastFavoriteCaseCustomer = 'sour fred';
+
+function upperCaseCustomerName() {
+    return customerName = customerName.toLocaleUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCaseCustomer = 'sour fred'
+}
